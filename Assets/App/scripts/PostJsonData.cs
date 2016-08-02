ba4ba3c817e5d5c7b3420713a842748dd7287467
Carlos Mondragon <carlosmondragon10@gmail.com>
2016-08-02 14:57:04 -0500

    first commit

---
+++ Assets/App/scripts/PostJsonData.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PostJsonData : MonoBehaviour {
+
+	// Use this for initialization
+	string Url;
+	void Start () {
+		Url = "localhost/webservice-kinect/webservice.php";
+		//PostData(100,"Unity");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PostData(int evento, string name, string time){
+		WWWForm dataParameters = new WWWForm ();
+		dataParameters.AddField ("name", name);
+		dataParameters.AddField ("event", evento);
+		dataParameters.AddField ("time", time);
+		Debug.Log ("User: "+name);
+		Debug.Log ("Event: "+evento);
+		Debug.Log ("Time: "+time);
+
+		WWW www = new WWW (Url,dataParameters);
+		StartCoroutine (PostdataEnumerator(www));
+
+	}
+
+	IEnumerator PostdataEnumerator(WWW www){
+		yield return www;
+		if(www.error == null){
+			Debug.Log("respuesta: " +www.text);
+		}else{
+			Debug.Log(www.error);
+		}
+	}
+}
---
+++ Assets/App/scripts/currentTime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class currentTime : MonoBehaviour {
+
+	// Use this for initialization
+
+	public DateTime time;
+
+	void Start () {
+
+		time = System.DateTime.Now;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time = System.DateTime.Now;
+	
+	}
+}
